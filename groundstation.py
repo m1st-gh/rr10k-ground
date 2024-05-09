141070156f392de999e36aedf9c812102eb34561
m1st-gh <93106448+m1st-gh@users.noreply.github.com>
2024-05-09 16:49:30 -0400

    Update .gitignore to include input.txt and .vscode/launch.json

--- groundstation.py
+++ groundstation.py
@@ -3,17 +3,20 @@ import threading
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import time
+import re
 
 found = False
 data = [[] for _ in range(10)]
 
 
-
 def listen_input(ser):
     global found
+    running = False
+    pattern = re.compile(r"^-?\d+(\.\d+)?(,-?\d+(\.\d+)?)*$", re.IGNORECASE)
     while True:
         response = ser.readline().decode().strip()
-        if response == "STARTING":
+        running = pattern.match(response)
+        if response == "STARTING" or running != None:
             found = True
             time.sleep(0.5)
             print("Found connection!")
@@ -34,7 +37,7 @@ def initialize_serial(port, baud_rate):
     try:
         ser = serial.Serial(port, baud_rate)
         return ser
-    except:
+    except serial.SerialException:
         print("Device Not detected.")
         exit(1)
 
@@ -59,28 +62,129 @@ def animate(frame, ser, axs):
     for ax in axs:
         for sub_ax in ax:
             sub_ax.clear()
+    ser.reset_input_buffer()
     indata = ser.readline().decode().strip()
     indata = indata.split(",")
-    print (indata)
+    print(indata)
     if len(indata) == 10:
         for i in range(10):
-            data[i].append(float(indata[i]))
-        if data[9][-1] - data[9][0] > 20:
-            for i in range(10):
-                data[i].pop(0)
-        for ax in axs:
-            for sub_ax in ax:
-                sub_ax.set_xlim(data[9][0], data[9][-1])
+            try:
+                data[i].append(float(indata[i]))
+            except ValueError:
+                data[i].append(-1)
+    def get_last_element(lst):
+        if lst:  # Check if the list is not empty
+            return lst[-1]
+        else:
+            return 0  # Or any default value
     
-    axs[0, 0].plot(data[9], data[0], label="x deg's", color="red")
-    axs[0, 0].plot(data[9], data[1], label="y deg's", color="blue")
-    axs[1, 0].plot(data[9], data[2], label="x g's", color="red")
-    axs[1, 0].plot(data[9], data[3], label="y g's", color="blue")
-    axs[1, 0].plot(data[9], data[4], label="z g's", color="green")
-    axs[2, 0].plot(data[9], data[5], label="Atm", color="blue")
-    axs[0, 1].plot(data[9], data[6], label="Temp C", color="black")
-    axs[1, 1].plot(data[9], data[7], label="Alt M", color="black")
-    axs[2, 1].plot(data[9], data[8], label="Head", color="black")
+    def draw_deg():
+        axs[0, 0].plot(data[9], data[0], label="x deg's", color="red")
+        axs[0, 0].plot(data[9], data[1], label="y deg's", color="blue")
+        axs[0, 0].set_xlim(data[9][0], data[9][-1])
+        axs[0, 0].legend(loc="upper left")
+        axs[0, 0].text(
+            0.9825,
+            0.95,
+            "\n".join(str(get_last_element(data[i])) for i in range(2)),
+            ha="right",
+            va="top",
+            transform=axs[0, 0].transAxes,
+            bbox=dict(facecolor="white", alpha=0.5),
+        )
+
+    def draw_g():
+        axs[1, 0].plot(data[9], data[2], label="x g's", color="red")
+        axs[1, 0].plot(data[9], data[3], label="y g's", color="blue")
+        axs[1, 0].plot(data[9], data[4], label="z g's", color="green")
+        axs[1, 0].set_xlim(data[9][0], data[9][-1])
+        axs[1, 0].legend(loc="upper left")
+        axs[1, 0].text(
+            0.9825,
+            0.95,
+            "\n".join(str(get_last_element(data[i])) for i in range(2, 5)),
+            ha="right",
+            va="top",
+            transform=axs[1, 0].transAxes,
+            bbox=dict(facecolor="white", alpha=0.5),
+        )
+
+    def draw_atm():
+        axs[2, 0].plot(data[9], data[5], label="Atm", color="blue")
+        axs[2, 0].set_xlim(data[9][0], data[9][-1])
+        axs[2, 0].legend(loc="upper left")
+        axs[2, 0].text(
+            0.9825,
+            0.95,
+            str(get_last_element(data[5])),
+            ha="right",
+            va="top",
+            transform=axs[2, 0].transAxes,
+            bbox=dict(facecolor="white", alpha=0.5),
+        )
+
+    def draw_temp():
+        axs[0, 1].plot(data[9], data[6], label="Temp C", color="black")
+        axs[0, 1].set_xlim(data[9][0], data[9][-1])
+        axs[0, 1].legend(loc="upper left")
+
+    axs[0, 1].text(
+        0.9825,
+        0.95,
+        str(get_last_element(data[6])),
+        ha="right",
+        va="top",
+        transform=axs[0, 1].transAxes,
+        bbox=dict(facecolor="white", alpha=0.5),
+    )
+
+    def draw_alt():
+        axs[1, 1].plot(data[9], data[7], label="Alt M", color="black")
+        axs[1, 1].set_xlim(data[9][0], data[9][-1])
+        axs[1, 1].legend(loc="upper left")
+
+    axs[1, 1].text(
+        0.9825,
+        0.95,
+        str(get_last_element(data[7])),
+        ha="right",
+        va="top",
+        transform=axs[1, 1].transAxes,
+        bbox=dict(facecolor="white", alpha=0.5),
+    )
+
+    def draw_head():
+        axs[2, 1].plot(data[9], data[8], label="Head", color="black")
+        axs[2, 1].set_xlim(data[9][0], data[9][-1])
+        axs[2, 1].legend(loc="upper left")
+        axs[2, 1].text(
+            0.9825,
+            0.95,
+            str(get_last_element(data[8])),
+            ha="right",
+            va="top",
+            transform=axs[2, 1].transAxes,
+            bbox=dict(facecolor="white", alpha=0.5),
+        )
+    threads = []
+    t1 = threading.Thread(target=draw_deg)
+    t2 = threading.Thread(target=draw_g)
+    t3 = threading.Thread(target=draw_atm)
+    t4 = threading.Thread(target=draw_temp)
+    t5 = threading.Thread(target=draw_alt)
+    t6 = threading.Thread(target=draw_head)
+    threads.extend([t1, t2, t3, t4, t5, t6])
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+def main():
+    ser = connect_serial()
+    start_reading(ser)
+    fig, axs = plt.subplots(3, 2)
     axs[0, 0].set_ylim(-190, 190)
     axs[0, 0].set_ylim(-190, 190)
     axs[1, 0].set_ylim(-16, 16)
@@ -90,23 +194,10 @@ def animate(frame, ser, axs):
     axs[0, 1].set_ylim(-40, 60)
     axs[1, 1].set_ylim(-100, 4000)
     axs[2, 1].set_ylim(0, 360)
-    for ax in axs:
-        for sub_ax in ax:
-            sub_ax.legend(loc = "upper left")
-    axs[0, 0].text(0.9825, 0.95, ('\n'.join((str(data[0][-1]), str(data[1][-1])))), ha='right', va='top', transform=axs[0, 0].transAxes, bbox=dict(facecolor='red', alpha=0.5))
-    axs[1, 0].text(0.9825, 0.95, ('\n'.join((str(data[2][-1]), str(data[3][-1]), str(data[4][-1])))), ha='right', va='top', transform=axs[1, 0].transAxes, bbox=dict(facecolor='red', alpha=0.5))
-    axs[2, 0].text(0.9825, 0.95, str(data[5][-1]), ha='right', va='top', transform=axs[2, 0].transAxes, bbox=dict(facecolor='red', alpha=0.5))
-    axs[0, 1].text(0.9825, 0.95, str(data[6][-1]), ha='right', va='top', transform=axs[0, 1].transAxes, bbox=dict(facecolor='red', alpha=0.5))
-    axs[1, 1].text(0.9825, 0.95, str(data[7][-1]), ha='right', va='top', transform=axs[1, 1].transAxes, bbox=dict(facecolor='red', alpha=0.5))
-    axs[2, 1].text(0.9825, 0.95, str(data[8][-1]), ha='right', va='top', transform=axs[2, 1].transAxes, bbox=dict(facecolor='red', alpha=0.5))
-
-def main():
-    ser = connect_serial()
-    start_reading(ser)
-    fig, axs = plt.subplots(3, 2)
-    ani = FuncAnimation(fig, animate, fargs=(ser, axs), interval=250, save_count=20)
+    update = FuncAnimation(
+        fig, animate, fargs=(ser, axs), interval=100, save_count=20, blit=False
+    )
     plt.show()
-    
 
 
 if __name__ == "__main__":
